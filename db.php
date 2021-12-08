c732dce62dd867eb4781536fae76a4965535bc77
winerboys <winerboys@gmail.com>
2021-12-07 23:13:20 -0500

    se agrego comentarios ala conexion a mysql

--- db.php
+++ db.php
@@ -2,10 +2,10 @@
 session_start();
 
 $conn = mysqli_connect(
-  'localhost',
-  'root',
-  'password123',
-  'php_mysql_crud'
+  'localhost', 
+  'root',     
+  'password123', 
+  'php_mysql_crud' 
 ) or die(mysqli_erro($mysqli));
 
 ?>
